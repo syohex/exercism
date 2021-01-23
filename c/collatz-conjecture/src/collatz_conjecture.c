1284f18ae13af313cb8c2ddfd1df1574a7c6e870
Shohei YOSHIDA <syohex@gmail.com>
2021-01-24 00:51:16 +0900

    Add collatz conjecture in C

---
+++ c/collatz-conjecture/src/collatz_conjecture.c
@@ -0,0 +1,20 @@
+#include "collatz_conjecture.h"
+
+int steps(int start) {
+    if (start <= 0) {
+        return ERROR_VALUE;
+    }
+
+    int count = 0;
+    while (start != 1) {
+        if (start % 2 == 0) {
+            start /= 2;
+        } else {
+            start = (3 * start) + 1;
+        }
+
+        ++count;
+    }
+
+    return count;
+}
