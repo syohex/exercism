929a7e6006d1f058282f8d9bf72ba08340c3c410
Shohei YOSHIDA <syohex@gmail.com>
2021-01-26 21:54:53 +0900

    Add space age in Rust

---
+++ rust/space-age/src/lib.rs
@@ -0,0 +1,78 @@
+// The code below is a stub. Just enough to satisfy the compiler.
+// In order to pass the tests you can add-to or change any of this code.
+
+#[derive(Debug)]
+pub struct Duration {
+    pub seconds: u64,
+}
+
+impl From<u64> for Duration {
+    fn from(s: u64) -> Self {
+        Self { seconds: s }
+    }
+}
+
+pub trait Planet {
+    fn earth_year() -> f64;
+
+    fn years_during(d: &Duration) -> f64 {
+        (d.seconds as f64) / (365.25 * Self::earth_year() * 24.0 * 3600.0)
+    }
+}
+
+pub struct Mercury;
+pub struct Venus;
+pub struct Earth;
+pub struct Mars;
+pub struct Jupiter;
+pub struct Saturn;
+pub struct Uranus;
+pub struct Neptune;
+
+impl Planet for Mercury {
+    fn earth_year() -> f64 {
+        0.2408467
+    }
+}
+
+impl Planet for Venus {
+    fn earth_year() -> f64 {
+        0.61519726
+    }
+}
+
+impl Planet for Earth {
+    fn earth_year() -> f64 {
+        1.0
+    }
+}
+
+impl Planet for Mars {
+    fn earth_year() -> f64 {
+        1.8808158
+    }
+}
+
+impl Planet for Jupiter {
+    fn earth_year() -> f64 {
+        11.862615
+    }
+}
+
+impl Planet for Saturn {
+    fn earth_year() -> f64 {
+        29.447498
+    }
+}
+
+impl Planet for Uranus {
+    fn earth_year() -> f64 {
+        84.016846
+    }
+}
+
+impl Planet for Neptune {
+    fn earth_year() -> f64 {
+        164.79132
+    }
+}
